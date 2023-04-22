8ac87f716c74fede9ae361219e2c8d0927fc3cdd
Pratheek Shetty <83195025+techshetty@users.noreply.github.com>
2023-04-22 13:50:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+function encrypt() {
+				var entext= document.getElementById("icipher").value;
+			var passkey= document.getElementById("ipass").value;
+			return CryptoJS.AES.encrypt(entext,passkey);
+	}
+	function decrypt(){
+			var entext= document.getElementById("icipher").value;
+			var passkey= document.getElementById("ipass").value;
+			var dec = CryptoJS.AES.decrypt(entext,passkey)
+		return dec.toString(CryptoJS.enc.Utf8);
 
+	}
+	function calc(){
+		var choice = document.getElementById("ende").value;
+		if(choice=="Encrypt"){
+			document.getElementById("outputbox").innerHTML = encrypt();
+		}
+		if(choice=="Decrypt"){
+			document.getElementById("outputbox").innerHTML = decrypt();
+		}
+	}
